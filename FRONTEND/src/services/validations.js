85029f058d68949a06411caf6c54465276102fbc
Gianni Gabriel <clondireanug@gmail.com>
2025-06-27 23:31:34 +0200

    feat:add display of messages for validation errors in forms

---
+++ FRONTEND/src/services/validations.js
@@ -0,0 +1,22 @@
+    export const validate = (formData) => {
+    const newErrors = {};
+
+    if (!formData.username.trim()) {
+      newErrors.username = "Username is requiered";
+    }else if (formData.username.length < 4 || formData.username.length > 100) {
+      newErrors.username = "Username must be between 4 and 100 characters";
+    }
+
+    if (!formData.password) {
+      newErrors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Password must be at least 6 characters long";
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      newErrors.confirmPassword = "Passwords do not match";
+    }
+    console.log(newErrors)
+    return newErrors;
+  };
+  
