b93d4448126541f18d0ccd28acb367a471e338e6
Gianni Gabriel <clondireanug@gmail.com>
2025-06-28 18:10:01 +0200

    feat: enhance login form validation and error handling

--- FRONTEND/src/pages/loginPage.jsx
+++ FRONTEND/src/pages/loginPage.jsx
@@ -9,15 +9,15 @@ export const LoginPage = () => {
   });
 
   const [errors, setErrors] = useState({});
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value }); 
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const validationErrors = validateLogin(formData); 
+    const validationErrors = validateLogin(formData);
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors);
       return;
@@ -40,71 +40,85 @@ export const LoginPage = () => {
       const data = await res.json();
       console.log(data);
 
-
-        if (res.ok) {
-            localStorage.setItem("token", data.token) //almacena el token en el localeStorage para identificar al usuario en la pagina
-            navigate("/"); 
-        } else {
-            setErrors({ general: data.errors[0].msg });
-            console.log(errors)
+      if (res.ok) {
+        localStorage.setItem("token", data.token); //almacena el token en el localeStorage para identificar al usuario en la pagina
+        navigate("/");
+      } else {
+        //si el server devuelve un array de errores, significa que el usario no existe, si en cambio delvuelve solo un mensaje significa que las creedenciales son incorrectas
+        if (data.errors && Array.isArray(data.errors)) {
+          setErrors({ username: data.errors[0].msg });
+        } else if (data.message) {
+          setErrors({ wrongCredentials: data.message });
         }
+
+        console.log(errors);
+      }
     } catch (err) {
       alert("Error: " + err.message);
     }
   };
 
-return (
-  <>
-    <h1 className="mt-3 mb-9 ">Login</h1>
-    <form
-      className="w-full max-w-screen-md mx-auto px-4"
-      onSubmit={handleSubmit}
-    >
-      <div className="mb-5">
-        <label
-          htmlFor="username"
-          className="block mb-2 text-xl font-medium text-gray-900 dark:text-white"
-        >
-          Username:
-        </label>
-        <input
-          type="text"
-          id="username"
-          onChange={handleChange}
-          value={formData.username}
-          className={`border ${
-            errors.username || errors.general ? "border-red-500" : "border-gray-600"
-          } text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
-          placeholder="Username"
-        />
-        {errors.username && <p className="text-red-500">{errors.username}</p>}
-        {errors.general && <p className="text-red-500">{errors.general}</p>}
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="password"
-          className="block mb-2 text-xl font-medium text-gray-900 dark:text-white"
-        >
-          Password:
-        </label>
-        <input
-          type="password"
-          id="password"
-          onChange={handleChange}
-          value={formData.password}
-          className={`border ${
-            errors.password ? "border-red-500" : "border-gray-600"
-          } text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
-        />
-        {errors.password && <p className="text-red-500">{errors.password}</p>}
-      </div>
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  return (
+    <>
+      <h1 className="mt-3 mb-9 ">Login</h1>
+      <form
+        className="w-full max-w-screen-md mx-auto px-4"
+        onSubmit={handleSubmit}
       >
-        Login
-      </button>
-    </form>
-  </>
-);
-}
+        <div className="mb-5">
+          <label
+            htmlFor="username"
+            className="block mb-2 text-xl font-medium text-gray-900 dark:text-white"
+          >
+            Username:
+          </label>
+          <input
+            type="text"
+            id="username"
+            onChange={handleChange}
+            value={formData.username}
+            className={`border ${
+              errors.username || errors.usernameNotFound
+                ? "border-red-500"
+                : "border-gray-600"
+            } text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+            placeholder="Username"
+          />
+          {errors.username && <p className="text-red-500">{errors.username}</p>}
+          {errors.usernameNotFound && (
+            <p className="text-red-500">{errors.usernameNotFound}</p>
+          )}
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-xl font-medium text-gray-900 dark:text-white"
+          >
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            onChange={handleChange}
+            value={formData.password}
+            className={`border ${
+              errors.password || errors.wrongCredentials
+                ? "border-red-500"
+                : "border-gray-600"
+            } text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+          />
+          {errors.password && <p className="text-red-500">{errors.password}</p>}
+          {errors.wrongCredentials && (
+            <p className="text-red-500">{errors.wrongCredentials}</p>
+          )}
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Login
+        </button>
+      </form>
+    </>
+  );
+};
